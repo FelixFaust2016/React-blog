edc641879d8b64947277eddefd164dacfd65349b
FelixFaust2016 <victoriheukwmere@gmail.com>
2020-08-03 04:06:21 -0700

    first-commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const config = require("./config/key");
 const { User } = require("./model/users");
+const { auth } = require("./middleware/auth");
 
 mongoose
   .connect(
@@ -20,12 +21,46 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+  });
+});
+
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-  user.save((err, userData) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+      userData: doc,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "wrong password" });
+      }
+    });
+    user.generateToken((err, user) => {
+      if (err) return res.status(200).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
     });
   });
 });
